804bfc17b466e70f2f57055425415262c596ef27
guilherme123456 <guilhermemluiz@gmail.com>
2022-01-25 19:45:24 -0300

    Criado algumas excecoes

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,12 +1,17 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
-		
+
+		if (rows <1 || columns <1) {
+
+			throw new BoardException("Erro ao criar tabuleiro, é necessário que haja pelo menos 1 linha e 1 coluna ");
+
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -14,31 +19,62 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
+
 	public Piece piece(int row,int column) {
-		
+
+		if (!positionExists(row, column)) {
+
+			throw new BoardException("Posição nao existe no tabuleiro");
+
+		}
+
 		return pieces[row][column];
 	}
 	public Piece piece(Position position) {
-		
+
+		if(!positionExists(position)) {
+
+			throw new BoardException("Posição nao existe no tabuleiro");
+		}
+
 		return pieces[position.getRow()][position.getColumn()];
-		
+
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+
+			throw new BoardException("There is a piece on position" + position);
+
+		}
 		pieces[position.getRow()][position.getColumn()]= piece;
 		piece.position = position;
-		
+
 	}
-	
-	
+	private boolean positionExists(int row,int column) {
+		return  row >= 0 && row < rows && column >= 0 && column < columns; 
+
+	}
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	public boolean thereIsAPiece(Position position) {
+
+		if(!positionExists(position)) {
+
+			throw new BoardException("Posição nao existe no tabuleiro");
+
+		}
+
+
+		return piece(position)!= null;
+
+	}
+
+
 
 }
